ed0c1a8091c358f5fd267567083101091c8ead42
william koller <wkoller25@gmail.com>
2021-06-21 20:41:18 -0300

    chore: add tslint

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
