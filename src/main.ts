7f94872e0d332d7b248d8fabb3ac3f4d48663abc
william koller <wkoller25@gmail.com>
2021-06-21 20:51:31 -0300

    refactor: add type NestApplication im app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(ApplicationModule);
   await app.listen(process.env.PORT);
 }
 bootstrap();
