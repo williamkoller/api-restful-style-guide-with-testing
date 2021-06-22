c94bc1f11faf0b4574dacafae5f3b44ddcab2b4e
william koller <wkoller25@gmail.com>
2021-06-21 21:49:03 -0300

    chore: add jest.e2e.js

---
+++ jest.e2e.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/apps/*/test/*.e2e-spec.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
