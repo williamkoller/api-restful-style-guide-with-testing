56e2a412cb263a2cbb86f8e37f0bf3dfef3ab95d
william koller <wkoller25@gmail.com>
2021-06-24 02:49:43 -0300

    test: remove imports

--- src/modules/app/app.module.spec.ts
+++ src/modules/app/app.module.spec.ts
@@ -1,16 +1,11 @@
-import { configService } from '@/infra/db/config/config.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppModule } from './app.module';
 
 describe('AppModule', () => {
   let appModule;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
-        AppModule,
-      ],
+      imports: [AppModule],
     }).compile();
     appModule = module.get<AppModule>(AppModule);
   });
