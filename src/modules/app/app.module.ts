340eac7a3f7a8fd9714ca15825fd14bbadcd57a8
william koller <wkoller25@gmail.com>
2021-06-23 23:51:40 -0300

    feat: add UserModule in AppModule

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,12 +1,14 @@
 import { configService } from '@/infra/db/config/config.service';
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '@/modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
+    forwardRef(() => UserModule),
   ],
   controllers: [],
   providers: [],
