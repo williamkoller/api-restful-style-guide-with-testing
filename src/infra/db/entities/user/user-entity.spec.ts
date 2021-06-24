2762923b8309cd0c1f5de20e147d6a77bb081b66
william koller <wkoller25@gmail.com>
2021-06-24 00:15:13 -0300

    test: should be called the class with the user class only name and surname

--- src/infra/db/entities/user/user-entity.spec.ts
+++ src/infra/db/entities/user/user-entity.spec.ts
@@ -1,4 +1,5 @@
 import TestUtilUser from '@/infra/db/entities/test/TestUtilUser';
+import { User } from './user-entity';
 
 describe('UserEntity class', () => {
   it('should be called with correct params', () => {
@@ -10,5 +11,14 @@ describe('UserEntity class', () => {
     expect(user.password).toBe('any_password');
     expect(user.createdAt).toEqual(new Date('2021-06-23T00:58:05.581Z'));
     expect(user.updatedAt).toEqual(new Date('2021-06-23T00:58:05.581Z'));
+  });
+
+  it('should be called the class with the user class only name and surname', () => {
+    const user = new User({ name: 'any_name', surname: 'any_surname' });
+    const userTest = TestUtilUser.mockUser();
+    expect(user).toBeTruthy();
+    expect(userTest).toBeTruthy();
+    expect(user.name).toBe(userTest.name);
+    expect(user.surname).toBe(userTest.surname);
   });
 });
