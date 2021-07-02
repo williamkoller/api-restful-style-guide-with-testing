3b230eb222002008ac430d0793e3659aab1e3fa7
william koller <wkoller25@gmail.com>
2021-07-01 22:13:10 -0300

    test: should be called with validate

--- src/modules/user/dtos/add-user/add-user.dto.spec.ts
+++ src/modules/user/dtos/add-user/add-user.dto.spec.ts
@@ -1,4 +1,5 @@
 import { AddUserDto } from './add-user.dto';
+import { validate } from 'class-validator';
 
 describe('AddUserDto', () => {
   it('should create AddUserDto object', () => {
@@ -8,10 +9,22 @@ describe('AddUserDto', () => {
       email: 'any_mail@email.com',
       password: 'any_password',
     };
+
     expect(addUserDto).toBeTruthy();
     expect(addUserDto.name).toBe('any_name');
     expect(addUserDto.surname).toBe('any_surname');
     expect(addUserDto.email).toBe('any_mail@email.com');
     expect(addUserDto.password).toBe('any_password');
+  });
+
+  it('should be called with validate', async () => {
+    const addUserDto: AddUserDto = {
+      name: 'any_name',
+      surname: 'any_surname',
+      email: 'any_mail@email.com',
+      password: 'any_password',
+    };
+    const error = await validate(addUserDto);
+    expect(error.length).toEqual(0);
   });
 });
