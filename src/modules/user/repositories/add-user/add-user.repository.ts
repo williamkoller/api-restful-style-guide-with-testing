b03f7df049550e222718d2edf90d517571202876
william koller <wkoller25@gmail.com>
2021-07-01 21:51:09 -0300

    feat: should be called save with correct params

--- src/modules/user/repositories/add-user/add-user.repository.ts
+++ src/modules/user/repositories/add-user/add-user.repository.ts
@@ -1,6 +1,12 @@
 import { User } from '@/infra/db/entities/user/user-entity';
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { AddUserDto } from '../../dtos/add-user/add-user.dto';
 
 Injectable();
-export class AddUserRepository extends Repository<User> {}
+export class AddUserRepository extends Repository<User> {
+  async addUser(data: AddUserDto): Promise<User> {
+    const newUser = this.create(data);
+    return await this.save(newUser);
+  }
+}
