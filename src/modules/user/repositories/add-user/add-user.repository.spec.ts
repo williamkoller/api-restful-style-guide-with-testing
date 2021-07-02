ce61f4af2b397999e047598f457b67c8b71ba9f1
william koller <wkoller25@gmail.com>
2021-07-01 21:50:45 -0300

    test: should be called save with correct params

--- src/modules/user/repositories/add-user/add-user.repository.spec.ts
+++ src/modules/user/repositories/add-user/add-user.repository.spec.ts
@@ -1,16 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AddUserRepository } from './add-user.repository';
+import TestUtilUser from '@/infra/db/entities/test/TestUtilUser';
 
 describe('AddUserRepository', () => {
-  let addUserRepository;
+  let addUserRepository: AddUserRepository;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AddUserRepository],
     }).compile();
 
     addUserRepository = module.get<AddUserRepository>(AddUserRepository);
+    addUserRepository.create = jest.fn();
+    addUserRepository.save = jest.fn();
   });
   it('should be defined', () => {
     expect(addUserRepository).toBeDefined();
+  });
+
+  it('should be called save with correct params', async () => {
+    const user = TestUtilUser.mockUser();
+    const newUser = (addUserRepository.create = jest
+      .fn()
+      .mockReturnValue(user));
+    addUserRepository.save = jest.fn().mockReturnValue(newUser);
+    await addUserRepository.addUser(user);
+    expect(addUserRepository.save).toBeCalledWith(user);
   });
 });
